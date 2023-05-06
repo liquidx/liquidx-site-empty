71cdf2357e2a0824fc42cf036f9b5de8d9ba93aa
Alastair Tse <alastair@liquidx.net>
2023-05-06 10:53:49 +0900

    Add a simple component example for everything to be based out of.

---
+++ src/lib/EmptyView.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let title = 'Page';
+</script>
+
+<div class="p-4 text-2xl">{title}</div>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,3 @@
 // Reexport your entry components here
+import EmptyView from "./EmptyView.svelte";
+export { EmptyView };
