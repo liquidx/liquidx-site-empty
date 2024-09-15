ecf4381465765cb1fef76736ba601c484e0ece2b
Alastair Tse <alastair@liquidx.net>
2024-09-15 15:26:34 +0900

    Add Locals

--- svelte-site/src/app.d.ts
+++ svelte-site/src/app.d.ts
@@ -1,12 +1,17 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { MongoClient } from "mongodb";
+
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      dbClient: MongoClient;
+
+    }
     // interface PageData {}
     // interface Platform {}
   }
 }
 
-export {};
+export { };
